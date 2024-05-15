43fe3b091fdc6469428a135c01c567fd2085b989
Danik <danikyesiam@gmail.com>
2024-05-15 05:31:26 +0300

    genera commit

--- js/test.js
+++ js/test.js
@@ -1,1 +1,10 @@
-f
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+let amountArrItem = data.length;
+
+for (let i = 0; i < data.length; i++) {
+    result[i] = data[amountArrItem - 1];
+    amountArrItem -= 1;
+}
+
+console.log(result);
