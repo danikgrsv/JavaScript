68a3ca0a017dd588452a404ae0ba6afba0132bfe
Danik <danikyesiam@gmail.com>
2024-05-22 18:04:06 +0300

    start work with scriptV3.js

---
+++ js/scriptV3.js
@@ -0,0 +1,19 @@
+// 'use strict';
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// // Главный объект
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false    
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренних фильмов?', 'Название фильма:');
+//     const b = prompt('Насколько баллов можете его оценить?', 'Оценка:');
+
+//     if ()
+// }
